d0a1494fa74567329fea96802190d7706c73e6f0
sudarsancse <sudarsansarkarcst@gmail.com>
2026-03-22 20:11:45 +0530

    Cart schema design

---
+++ services/restaurant/src/models/Cart.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document } from "mongoose";
+import restaurant from "./restaurant.js";
+
+export interface ICart extends Document {
+  userId: mongoose.Types.ObjectId;
+  restaurantId: mongoose.Types.ObjectId;
+  itemId: mongoose.Types.ObjectId;
+  quauntity: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const schema = new Schema<ICart>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    restaurantId: {
+      type: Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+      index: true,
+    },
+    itemId: {
+      type: Schema.Types.ObjectId,
+      ref: "MenuItem",
+      required: true,
+      index: true,
+    },
+    quauntity: {
+      type: Number,
+      default: 1,
+      min: 1,
+    },
+  },
+  { timestamps: true },
+);
+
+schema.index({ useId: 1, restaurantId: 1, itemId: 1 }, { unique: true });
+
+export default mongoose.model<ICart>("Cart", schema);
